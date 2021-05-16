e282867d74aba40060f6143a46ebdde4dfcc267a
Emanuel Rodrigues <emanuelrootsneite@gmail.com>
2021-05-16 15:43:37 -0300

    Initial commit

---
+++ app/src/main/java/com/emanuel/nybooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,16 @@
+package com.emanuel.nybooks.presentation.books
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.emanuel.nybooks.R
+import kotlinx.android.synthetic.main.activity_books.*
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+
+        toolbarMain.title = "Books"
+        setSupportActionBar(toolbarMain)
+    }
+}
