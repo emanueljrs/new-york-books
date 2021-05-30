759b563f19b40e4186d1a7b42ff3d4a03d809789
Emanuel Rodrigues <emanuelrootsneite@gmail.com>
2021-05-30 13:03:49 -0300

    Created and configured class BooksDetailsActivity and layout activity_books_details.xml

---
+++ app/src/main/java/com/emanuel/nybooks/presentation/details/BooksDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.emanuel.nybooks.presentation.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BooksDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books_details)
+    }
+}
